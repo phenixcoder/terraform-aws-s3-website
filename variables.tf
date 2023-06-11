1a7a7082c71dee896b8e41305f0e402a1d66010f
Balwant <balwant.matharu@gmail.com>
2023-06-11 11:43:01 +1000

    Added suprort for sub domain

--- variables.tf
+++ variables.tf
@@ -9,6 +9,12 @@ variable "name" {
   }
 }
 
+variable "hosting_zone_domain_name" {
+  type        = string
+  description = "Domain for fetching Hosting Zone ID form Route53. Required for DNS validation. If not passed, FQDN will be used to get Hosting Zone"
+  default     = ""
+}
+
 variable "hosting_zone_public" {
   type        = bool
   description = "Default true. Set false if hosted zone is private"
