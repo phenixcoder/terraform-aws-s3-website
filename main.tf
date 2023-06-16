9e88fbf90d735d58f1b241f2f960872139631ef2
Balwant <balwant.matharu@gmail.com>
2023-06-16 12:52:27 +1000

    Added aws_s3_bucket_public_access_block with false values

--- main.tf
+++ main.tf
@@ -59,6 +59,15 @@ data "aws_iam_policy_document" "CDNReadForGetBucketObjects" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 
 
 ############################################################
